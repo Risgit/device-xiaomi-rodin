f6249931746b7a6d021e51e272d673a715e25e65
Risgit <auwieder@ya.ru>
2025-05-03 10:30:32 +0300

    Adding files
    Adding files

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,9 +1,9 @@
-# Android device tree for Android mainline (generic)
-
-```
 #
 # Copyright (C) 2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := $(call my-dir)
+
+# Recovery init configuration files
